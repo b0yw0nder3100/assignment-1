ce00558cd754c2f47fde2ae1bd410ec6f866dc91
Martins Godwin Ikpe <75488009+b0yw0nder3100@users.noreply.github.com>
2021-02-06 23:41:00 +0100

    Add files via upload

---
+++ WindowsFormsApp1bmi/WindowsFormsApp1bmi/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace WindowsFormsApp1bmi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.radKg = new System.Windows.Forms.RadioButton();
+            this.radLb = new System.Windows.Forms.RadioButton();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radCm = new System.Windows.Forms.RadioButton();
+            this.radMe = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Weight";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Height";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 254);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Result";
+            // 
+            // txtResult
+            // 
+            this.txtResult.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.txtResult.Location = new System.Drawing.Point(131, 249);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(219, 56);
+            this.txtResult.TabIndex = 4;
+            this.txtResult.TextChanged += new System.EventHandler(this.TxtResult_TextChanged);
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(131, 163);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(219, 22);
+            this.txtHeight.TabIndex = 5;
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(131, 86);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(219, 22);
+            this.txtWeight.TabIndex = 6;
+            this.txtWeight.TextChanged += new System.EventHandler(this.TxtWeight_TextChanged);
+            // 
+            // radKg
+            // 
+            this.radKg.AutoSize = true;
+            this.radKg.Location = new System.Drawing.Point(131, 125);
+            this.radKg.Name = "radKg";
+            this.radKg.Size = new System.Drawing.Size(84, 21);
+            this.radKg.TabIndex = 8;
+            this.radKg.TabStop = true;
+            this.radKg.Text = "Kilogram";
+            this.radKg.UseVisualStyleBackColor = true;
+            // 
+            // radLb
+            // 
+            this.radLb.AutoSize = true;
+            this.radLb.Location = new System.Drawing.Point(234, 125);
+            this.radLb.Name = "radLb";
+            this.radLb.Size = new System.Drawing.Size(70, 21);
+            this.radLb.TabIndex = 9;
+            this.radLb.TabStop = true;
+            this.radLb.Text = "Pound";
+            this.radLb.UseVisualStyleBackColor = true;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.SystemColors.Control;
+            this.btnCalculate.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCalculate.Location = new System.Drawing.Point(68, 323);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(86, 36);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.SystemColors.Control;
+            this.btnReset.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnReset.Location = new System.Drawing.Point(264, 323);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(86, 36);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(252, 404);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(98, 34);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(12, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(421, 60);
+            this.panel1.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(30, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(361, 61);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "BMI Calculator";
+            // 
+            // radCm
+            // 
+            this.radCm.AutoSize = true;
+            this.radCm.Location = new System.Drawing.Point(131, 200);
+            this.radCm.Name = "radCm";
+            this.radCm.Size = new System.Drawing.Size(97, 21);
+            this.radCm.TabIndex = 14;
+            this.radCm.TabStop = true;
+            this.radCm.Text = "Centimetre";
+            this.radCm.UseVisualStyleBackColor = true;
+            this.radCm.CheckedChanged += new System.EventHandler(this.RadCm_CheckedChanged);
+            // 
+            // radMe
+            // 
+            this.radMe.AutoSize = true;
+            this.radMe.Location = new System.Drawing.Point(241, 200);
+            this.radMe.Name = "radMe";
+            this.radMe.Size = new System.Drawing.Size(65, 21);
+            this.radMe.TabIndex = 15;
+            this.radMe.TabStop = true;
+            this.radMe.Text = "Metre";
+            this.radMe.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 450);
+            this.Controls.Add(this.radMe);
+            this.Controls.Add(this.radCm);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.radLb);
+            this.Controls.Add(this.radKg);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.RadioButton radKg;
+        private System.Windows.Forms.RadioButton radLb;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radCm;
+        private System.Windows.Forms.RadioButton radMe;
+    }
+}
+
---
+++ WindowsFormsApp1bmi/WindowsFormsApp1bmi/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1bmi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TxtResult_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            double weight = Convert.ToDouble(txtWeight.Text);
+            double height = Convert.ToDouble(txtHeight.Text);
+            double result = 0.0;
+            string result2 = "";
+
+            if (radKg.Checked)
+            {
+             result = weight / (height * height);
+            }
+            else
+            if (radLb.Checked)                  
+             {
+                weight = weight / 2.205;
+                result = weight / (height * height);
+            }
+                       
+
+            if (result < 18.5)
+            {
+                result2 = "underweight";
+            }
+            else
+            if (result < 25)
+            {
+                result2 = "normal weight";
+            }
+            else
+            if (result < 30)
+            {
+                result2 = "overweight";
+            }
+            else
+            if (result > 30)
+                result2 = "youobese";
+
+;
+
+            txtResult.Text = "Your BMI is " + result.ToString("#.#") + "\r\n" + result2;
+
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            txtHeight.Text = "";
+            txtWeight.Text = "";
+            txtResult.Text = "";
+
+            radKg.Checked = false;
+            radLb.Checked = false;
+          
+        }
+
+        private void BtnExit_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TxtWeight_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadCm_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
